8263c0f458787266055dccd97362fffae54cecaf
Pavan Koppula <145316378+pavan-koppula@users.noreply.github.com>
2024-02-06 23:39:48 +0530

    Create Average_of_1_10.java

---
+++ Average_of_1_10.java
@@ -0,0 +1,17 @@
+// WAP to print 1 to 10 numbers and find its average
+import java.util.*;
+class Avg_10 {
+    public static void main(String[] args) {
+        int ar[]=new int[10];   //Array declaration
+        int sum=0;
+        for(int i=0;i<10;i++){  //1,2,3,4,5,6,78,9,10
+            ar[i]=i+1;          //ar[0,1,2,3,4,5,...9]=1,2,3,4,5,6,7,8,9,10
+            sum=sum+ar[i];
+        }
+        System.out.println(Arrays.toString(ar));    //Generate a string representation of an array(To print the contents of an array)
+        System.out.println("Sum of the Numbers in the array: "+sum);
+//To find average of 1 to 10
+        int avg=sum/(ar.length/2);
+        System.out.println("Average of given Numbers: "+avg);
+    }
+}
